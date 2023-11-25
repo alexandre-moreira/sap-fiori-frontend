351e7db54e9c984ed29c17427a3fd53bc7f8516c
Alexandre Moreira <4lexandre.moreira@gmail.com>
2023-11-25 13:23:39 -0300

    feat: integracao com api

--- webapp/controller/Inicio.controller.js
+++ webapp/controller/Inicio.controller.js
@@ -9,7 +9,56 @@ sap.ui.define([
 
         return Controller.extend("projetonetflix.controller.Inicio", {
             onInit: function () {
+                // Equivale ao Initialization do ABAP.
+                // Evento que é executado quando o programa é iniciado.
 
+                // Trabalhando com variáveis
+
+                // Variaveis de texto
+                    var serie = "Os Trapalhoes";
+
+                //Variaveis numericas
+                    var ano = 1980
+                
+                // Variaveis de lista de valores
+                // Equivale a tabela interna no ABAP
+                // Se chama Array no Javascript.
+                    var elenco = ["Didi", "Dede","Mussum", "Zacarias"]
+
+                    console.log(serie);
+                    console.log(ano);
+                    console.log(elenco);
+
+                // Variavel do tipo objeto (Variavel composta por varias propriedades)
+                // Equivale a estrutura no ABAP.
+                    var liveAction = {
+                        nome: "One Piece",
+                        ano: 2023,
+                        elenco: ['Luffy', 'Zoro', 'Nami', 'Usopp']
+                    }
+                    console.log(liveAction)
+                    
+                    var minhaLista = [
+                        {
+                            nome: "One Piece",
+                            ano: 2023,
+                            elenco: ['Luffy', 'Zoro', 'Nami', 'Usopp']
+                        },
+                        {
+                            nome: "Trapalhoes",
+                            ano: 1985,
+                            elenco: ["Didi", "Dede","Mussum", "Zacarias"]
+                        }
+                    ]
+
+                    console.log(minhaLista)
+
+            },
+            onPressLinkInicio: function(){
+                alert("Voce clicou no link Inicio");
+            },
+            onPressLinkSeries: function(){
+                alert("Voce clicou no link Séries");
             }
         });
     });
